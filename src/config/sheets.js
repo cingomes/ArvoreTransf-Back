3b46f0a25c41c514427cc6d0fb72a1c4b2007314
Eric-Altao <ericaltao13@gmail.com>
2024-09-23 07:13:07 -0300

    config spreadsheet

--- src/config/sheets.js
+++ src/config/sheets.js
@@ -5,23 +5,23 @@ const { config } = require('dotenv');
 config();
 
 async function accessSpreadsheet() {
-    const creds = new JWT({
-        email: process.env.GOOGLE_CLIENT_EMAIL,
-        key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        scopes:['https://www.googleapis.com/auth/spreadsheets']
-    });
-    const doc = new GoogleSpreadsheet('1CSSAOvqKQKUVQg7dd1atnrxzZIiyFOxDrqvUUD53CUg', creds);
-    
-    await doc.loadInfo(); 
-    return doc;
+  const auth = new JWT({
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  
+  const doc = new GoogleSpreadsheet('1jTFZwbLNVorHE40o1Rg2DAgHqbJnL1ipHnm4foNsTfA', auth);
+
+  await doc.loadInfo(); 
+  return doc;
 }
 
 async function testConnection() {
-    try {
-        const doc = await accessSpreadsheet();
-        console.log(`Conexão com a planilha ${doc.title} estabelecida`);
-    } catch (error) {
-        console.error('Erro ao conectar à planilha:', error);
-    }
+  try {
+    const doc = await accessSpreadsheet();
+    console.log(`Conexão com a planilha ${doc.title} estabelecida`);
+  } catch (error) {
+    console.error('Erro ao conectar à planilha:', error);
+  }
 }
-module.exports = { accessSpreadsheet, testConnection };
